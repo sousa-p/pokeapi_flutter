12a4ff04800a7c57ec348f77194d5913185bc7b4
sousa-p <p.soousa7@gmail.com>
2023-12-30 16:36:42 -0300

    Add more files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pokeapi_flutter/views/index_view.dart';
+import 'package:pokeapi_flutter/src/views/index_view.dart';
 
 void main() {
   runApp(const IndexView());
