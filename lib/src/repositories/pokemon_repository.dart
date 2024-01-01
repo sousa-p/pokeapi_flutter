3818e82b4ffa7d10bb73e6ade8b8ef60fcd463ea
sousa-p <p.soousa7@gmail.com>
2024-01-01 16:43:43 -0300

    Feature pokemon repository

--- lib/src/repositories/pokemon_repository.dart
+++ lib/src/repositories/pokemon_repository.dart
@@ -1,1 +1,28 @@
-class PokemonRepository {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pokeapi_flutter/src/models/pokemon_model.dart';
+
+class PokemonRepository {
+  final String baseUrl = 'pokeapi.co';
+
+  Future<List<PokemonModel>> getPokemons({int offset = 0, int limit = 20}) async {
+    String endpoint = '/api/v2/pokemon';
+    Map<String, dynamic> params = {'offset': offset.toString(), 'limit': limit.toString()};
+    Uri url = Uri.https(baseUrl, endpoint, params);
+
+    var response = await http.get(url);
+    var list = jsonDecode(response.body);
+    var results = list['results'] as List;
+    return results.map((e) => PokemonModel.fromJson(e)).toList();
+  }
+
+  Future<PokemonModel> getPokemon(dynamic search) async {
+    String endpoint = '/api/v2/pokemon/$search';
+    Uri url = Uri.https(baseUrl, endpoint);
+
+    var response = await http.get(url);
+    var json = jsonDecode(response.body);
+    return PokemonModel.fromJson(json);
+  }
+}
